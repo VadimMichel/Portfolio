32033651f09249b26360b1e07d0fddcd464fe7fe
VadimMichel <vadimmichel5@gmail.com>
2025-06-03 10:35:34 +0200

    feat: add my skills section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent],
+  imports: [LandingPageComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
-
+  
 }
---
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+interface Logo{
+  name: string;
+  imgUrl: string;
+}
+
+@Component({
+  selector: 'app-my-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-skills.component.html',
+  styleUrl: './my-skills.component.scss'
+})
+export class MySkillsComponent {
+logos : Logo[] = [
+    {name: 'Angular',
+      imgUrl: 'assets/img/logos/angular_logo.png'
+    },
+    {name: 'TypeScript',
+      imgUrl: 'assets/img/logos/type_script_logo.png'
+    },
+    {name: 'JavaScript',
+      imgUrl: 'assets/img/logos/java_script_logo.png'
+    },
+    {name: 'HTML',
+      imgUrl: 'assets/img/logos/html_logo.png'
+    },
+    {name: 'CSS',
+      imgUrl: 'assets/img/logos/css_logo.png'
+    },
+    {name: 'Firebase',
+      imgUrl: 'assets/img/logos/firebase_logo.png'
+    },
+    {name: 'Git',
+      imgUrl: 'assets/img/logos/git_logo.png'
+    },
+    {name: 'Rest Api',
+      imgUrl: 'assets/img/logos/api_logo.png'
+    },
+  ]
+}
