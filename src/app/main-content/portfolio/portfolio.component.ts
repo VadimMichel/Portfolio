a83c0f259abed9231ba678188aa668c332c65cef
VadimMichel <vadimmichel5@gmail.com>
2025-06-04 20:09:05 +0200

    feat: display all projects

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { ProjectComponent } from './project/project.component';
 
+export interface Project{
+  name : string;
+  imgaeUrl : string;
+  usedSkills : string;
+  description : string;
+  github : string;
+  rowRewerse : boolean;
+}
+
 @Component({
   selector: 'app-portfolio',
   standalone: true,
@@ -9,5 +18,23 @@ import { ProjectComponent } from './project/project.component';
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  projects : Project[] = [
+    {
+    name : 'Join',
+    imgaeUrl : 'assets/img/join.png',
+    usedSkills : 'Angulat | TypeScript | HTML | CSS | Firebase',
+    description : 'Task manager inspired by the Kanban System. Create and organize tasks using and drop functions, assign useres and categories.',
+    github : '#',
+    rowRewerse : false
+    },
+    {
+    name : 'El Pollo Loco',
+    imgaeUrl : 'assets/img/el_pollo_loco.png',
+    usedSkills : 'JavaScript | HTML | CSS ',
+    description : 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the chicken.',
+    github : '#',
+    rowRewerse : true
+    }
+  ]
+  
 }
--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { PortfolioComponent, Project } from '../portfolio.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PortfolioComponent, FormsModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
-
+  @Input()projectData!:Project;
+  @Input() index!: number;
+  @Input() totalProjects!: number;
 }
