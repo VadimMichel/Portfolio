a7dc56f469ebd1e9111e5242820dde26e8df6123
VadimMichel <vadimmichel5@gmail.com>
2025-06-06 12:02:50 +0200

    refactor: section margin left and right

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, NavBarComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
