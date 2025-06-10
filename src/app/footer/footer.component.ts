ee0d94d5638eab4f41a3b9cdfa96d805e6f49ecd
VadimMichel <vadimmichel5@gmail.com>
2025-06-10 19:54:08 +0200

    feat: translate footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {TranslatePipe, TranslateDirective} from "@ngx-translate/core"
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
