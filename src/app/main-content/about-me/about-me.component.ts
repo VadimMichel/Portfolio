6e5f8bd548b8485c703b3cdc6270a1f6b206d2e4
VadimMichel <vadimmichel5@gmail.com>
2025-06-10 14:27:42 +0200

    feat: translate about me

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
+
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, TranslatePipe, TranslateDirective],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
