703aa988e64d26b7ca97fec451ace9597403ca50
VadimMichel <vadimmichel5@gmail.com>
2025-06-10 13:56:30 +0200

    feat: translate en and de

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,26 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import {
+    TranslateService,
+    TranslatePipe,
+    TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, NavBarComponent, FooterComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, NavBarComponent, FooterComponent, AboutMeComponent, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
   title = 'portfolio';
 }
 
--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
