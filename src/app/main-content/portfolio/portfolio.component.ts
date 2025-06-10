17e9c3d1f196e2f0dee5c07b100c4eba8a5754f7
VadimMichel <vadimmichel5@gmail.com>
2025-06-10 17:59:00 +0200

    feat: translate portfolio component

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProjectComponent } from './project/project.component';
+import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
 
 export interface Project{
   name : string;
@@ -13,7 +14,7 @@ export interface Project{
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ProjectComponent],
+  imports: [ProjectComponent, TranslatePipe, TranslateDirective],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
@@ -23,7 +24,7 @@ export class PortfolioComponent {
     name : 'Join',
     imgaeUrl : 'assets/img/join.png',
     usedSkills : 'Angulat | TypeScript | HTML | CSS | Firebase',
-    description : 'Task manager inspired by the Kanban System. Create and organize tasks using and drop functions, assign useres and categories.',
+    description : 'portfolio.project1Description',
     github : '#',
     rowRewerse : false
     },
@@ -31,7 +32,7 @@ export class PortfolioComponent {
     name : 'El Pollo Loco',
     imgaeUrl : 'assets/img/el_pollo_loco.png',
     usedSkills : 'JavaScript | HTML | CSS ',
-    description : 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the chicken.',
+    description : 'portfolio.project2Description',
     github : '#',
     rowRewerse : true
     }
--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { PortfolioComponent, Project } from '../portfolio.component';
 import { FormsModule } from '@angular/forms';
+import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [CommonModule, PortfolioComponent, FormsModule],
+  imports: [CommonModule, PortfolioComponent, FormsModule, TranslatePipe, TranslateDirective],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
