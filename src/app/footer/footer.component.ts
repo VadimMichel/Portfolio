ef1fb24981cc45404acc186733c453968720df34
VadimMichel <vadimmichel5@gmail.com>
2025-07-22 15:59:26 +0200

    feat: show validation message and warning image for invalid inputs

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {TranslatePipe, TranslateDirective} from "@ngx-translate/core"
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [TranslatePipe, TranslateDirective, FormsModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  userName: string = "";
+  eMail: string = "";
+  message: string = "";
 }
