c79bbec4135172866e0de93de87fc62ebf9f0490
VadimMichel <vadimmichel5@gmail.com>
2025-09-16 16:27:40 +0200

    feat: provide and inject HttpClient and implement onSubmit function

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,7 @@ export const appConfig: ApplicationConfig = {
         useFactory: httpLoaderFactory,
         deps: [HttpClient],
       },
-    })
+    }),
+    provideHttpClient()
   ]
 };
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import {TranslatePipe, TranslateDirective} from "@ngx-translate/core"
 
 @Component({
@@ -11,11 +12,51 @@ import {TranslatePipe, TranslateDirective} from "@ngx-translate/core"
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  userName: string = "";
-  eMail: string = "";
-  message: string = "";
+  contactData :{ userName : string;
+    eMail : string;
+    message : string;
+  } = {
+    userName : "",
+    eMail : "",
+    message : ""
+  }
+
   usernameFocus: boolean = false;
   emailFocus: boolean = false;
   messageFocus: boolean = false;
   acceptedPrivacy: boolean = false;
+
+  mailTest = true;
+
+  http = inject(HttpClient);
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
 }
