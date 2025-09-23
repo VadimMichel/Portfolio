462490401eb7023e11d1de6f94b10e0a23515fc0
VadimMichel <vadimmichel5@gmail.com>
2025-09-23 16:06:38 +0200

    feat: add legal notice translation

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe, TranslateDirective } from "@ngx-translate/core"
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
