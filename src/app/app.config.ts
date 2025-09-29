ed300e8d30a4d77512c0ebd1b3f6ab8965f1651b
VadimMichel <vadimmichel5@gmail.com>
2025-09-29 09:18:47 +0200

    feat: enable memory scrolling for router links

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import {provideHttpClient} from "@angular/common/http";
 import {provideTranslateService, TranslateLoader} from "@ngx-translate/core";
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient} from '@angular/common/http';
+import { routes } from './app.routes';
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
     new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
-import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [provideRouter(routes,
+    withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled', // scrollt bei Navigation nach oben
+        anchorScrolling: 'enabled',           // erlaubt Sprünge zu #anchor
+      })
+  ),
     provideHttpClient(),
     provideTranslateService({
       loader: {
