eacace7dab8adebd60da0a7acdbd911cfa8bddb6
VadimMichel <vadimmichel5@gmail.com>
2025-10-04 16:23:28 +0200

    feat: add navigation links to all main sections from header

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,40 +1,52 @@
 import { Component, Renderer2, OnDestroy } from '@angular/core';
-import { TranslateService, TranslatePipe, TranslateDirective } from '@ngx-translate/core';
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective,
+} from '@ngx-translate/core';
 import { SecondFooterComponent } from '../second-footer/second-footer.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [TranslatePipe ,TranslateDirective, SecondFooterComponent],
+  imports: [TranslatePipe, TranslateDirective, SecondFooterComponent],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
   isMenuOpen: boolean = false;
   defaultLanguage: boolean = true;
 
-  constructor(private renderer: Renderer2, private translate: TranslateService) {}
-  
-  changeLanguage(language: string){
-    this.translate.use(language)
-    if(language == 'en'){
+  constructor(
+    private renderer: Renderer2,
+    private translate: TranslateService,
+    private router: Router
+  ) {}
+
+  changeLanguage(language: string) {
+    this.translate.use(language);
+    if (language == 'en') {
       this.defaultLanguage = true;
-    }else{
+    } else {
       this.defaultLanguage = false;
     }
   }
 
-  toggleMenu() {
-    if(this.isMenuOpen == false){
+  toggleMenu(id: string) {
+    if (this.isMenuOpen == false) {
       this.isMenuOpen = true;
       this.updateBodyScroll();
-    }else{
+    } else if (this.isMenuOpen == true) {
       this.isMenuOpen = false;
       this.updateBodyScroll();
+      if(id != ''){
+        this.scrollToId(id);
+      }
     }
   }
 
-    private updateBodyScroll() {
+  private updateBodyScroll() {
     if (this.isMenuOpen) {
       this.renderer.addClass(document.body, 'no-scroll');
     } else {
@@ -45,4 +57,19 @@ export class NavBarComponent {
   ngOnDestroy() {
     this.renderer.removeClass(document.body, 'no-scroll');
   }
+
+  scrollToId(id: string): void {
+  this.router.navigate(['/'], { fragment: id }).then(() => {
+    
+    setTimeout(() => {
+      const el = document.getElementById(id);
+      if (el) {
+        const style = getComputedStyle(el);
+        const marginTop = parseInt(style.marginTop, 10) || 0;
+        const top = el.getBoundingClientRect().top + window.scrollY - marginTop;
+        window.scrollTo({ top, behavior: 'smooth' });
+      }
+    }, 50);
+  });
+}
 }
